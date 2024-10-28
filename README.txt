24ba5025c7644469fbd9303523529de65a94a525
Barlas Söker <bsoker98@gmail.com>
2024-10-28 18:28:11 +0300

    Implemented the basic functionalities of the app, configured conan,cmake and ninja appropriately

---
+++ README.txt
@@ -0,0 +1,2 @@
+conan install . -s build_type=Release --build=missing --output-folder=build
+cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAINFILE="conan_toolchain.cmake" // toolchain flag can be removed because it is also specified at the cmakelists.txt
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+// src/main.cpp
+#include <httplib/httplib.h>
 
 int main() {
-    std::cout << "Hello Worlasdasdasdasdd!";
+    httplib::Server svr;
+    int port = 8080;
+
+    svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("Hello, World!", "text/plain");
+    });
+
+    std::cout << "Server is running on http://localhost:" << port << std::endl;
+    // Start the server on port 8080
+    svr.listen("0.0.0.0", port);
 
     return 0;
 }
